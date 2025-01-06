344ede32788c4b0154d023eef01c7b3a9a553f5d
Joshua Hickey <joshuah775@gmail.com>
2025-01-06 22:01:14 +0000

    Initial Setup Commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Glacial Indifference', 'sans-serif'],
+			}
+		},
+		colors: {
+			'peach': '#FFB777',
+			'forest': '#2D4F3C'
+		}
+	},
+	plugins: [],
+}
