ce806b50d08c104b38e1dd86c3a7c98de722739f
Joshua Hickey <joshuah775@gmail.com>
2025-01-11 17:25:04 +0000

    Formatting

--- src/animations.ts
+++ src/animations.ts
@@ -1,64 +1,71 @@
-import { animate, stagger } from 'motion'
-import type { Easing } from 'motion'
+import { animate, stagger, scroll } from "motion";
+import type { Easing } from "motion";
 
 type TransitionSettings = {
-    duration?: number
-    easing?: Easing | Easing[]
-    delay?: number
-    staggerChildren?: number
-}
+  duration?: number;
+  easing?: Easing | Easing[];
+  delay?: number;
+  staggerChildren?: number;
+};
 
 //When inputting the elements, make sure you use the querySelectorAll method to select all the elements you want to animate.
 
- export function animateLogo() {
-    const logo = document.querySelectorAll('.image-container')
-    
-    animate(
-        logo,
-        {
-            scale: [0.5, 1],
-            rotate: [-90, 0],
-        },
-        {
-            scale: { duration: 1, ease: 'easeOut' },
-            rotate: { duration: 1, ease: 'easeOut' },
-        },
-    )
+export function animateLogo() {
+  const logo = document.querySelectorAll(".image-container");
+
+  animate(
+    logo,
+    {
+      scale: [0.5, 1],
+      rotate: [-90, 0],
+    },
+    {
+      scale: { duration: 1, ease: "easeOut" },
+      rotate: { duration: 1, ease: "easeOut" },
+    }
+  );
 }
 
- export function animateFadeUp(elements: NodeListOf<Element>, startingY: number, transitionSettings: TransitionSettings, blur = true) {
+export function animateFadeUp(
+  elements: NodeListOf<Element>,
+  startingY: number,
+  transitionSettings: TransitionSettings,
+  blur = true
+) {
+  const { duration, staggerChildren, delay, easing } = transitionSettings;
 
-    const { duration, staggerChildren, delay, easing } = transitionSettings
-    
-    animate(
-        elements,
-        {
-            opacity: [0, 1],
-            y: [startingY, 0],
-            filter: [`blur(${blur ? 10 : 0}px)`, 'blur(0px)'],
-        },
-        {
-            duration: duration ?? 0.5,
-            ease: easing ?? 'easeOut',
-            delay: stagger(staggerChildren ?? 0, { startDelay: delay ?? 0}),
-        },
-    )
+  animate(
+    elements,
+    {
+      opacity: [0, 1],
+      y: [startingY, 0],
+      filter: [`blur(${blur ? 10 : 0}px)`, "blur(0px)"],
+    },
+    {
+      duration: duration ?? 0.5,
+      ease: easing ?? "easeOut",
+      delay: stagger(staggerChildren ?? 0, { startDelay: delay ?? 0 }),
+    }
+  );
 }
 
-export function animateTranslateY(elements: NodeListOf<Element>, translateY: number, transitionSettings: TransitionSettings) {
+export function animateTranslateY(
+  elements: NodeListOf<Element>,
+  translateY: number,
+  transitionSettings: TransitionSettings
+) {
+  const { duration, staggerChildren, delay, easing } = transitionSettings;
 
-    const { duration, staggerChildren, delay, easing } = transitionSettings
-
-    animate(
-        elements,
-        {
-            opacity: [0, 1],
-            y: [translateY, 0],
-        },
-        {
-            duration: duration ?? 0.5,
-            ease: easing ?? 'easeOut',
-            delay: stagger((staggerChildren ?? 0), { startDelay: delay ?? 0 }),
-        },
-    )
+  animate(
+    elements,
+    {
+      opacity: [0, 1],
+      y: [translateY, 0],
+    },
+    {
+      duration: duration ?? 0.5,
+      ease: easing ?? "easeOut",
+      delay: stagger(staggerChildren ?? 0, { startDelay: delay ?? 0 }),
+    }
+  );
 }
