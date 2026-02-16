3864cb5a8b465978cdc92296168cd19e4b9f5547
Joshua Hickey <joshuah775@gmail.com>
2026-02-16 02:20:44 +0000

    trying this

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,6 +18,9 @@ export default defineConfig({
   },
 
   adapter: cloudflare({
-    imageService: 'cloudflare'
+    imageService: 'cloudflare',
+    platformProxy: {
+      enabled: true
+    }
   })
 });
